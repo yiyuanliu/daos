fc3f265efa9a06e1e20e83dcf82704bf05daa5ac
Michael MacDonald <mjmac.macdonald@intel.com>
2021-02-08 15:07:47 -0500

    DAOS-3798 test: Add MS resilience test
    Verify that a replicated MS can survive N failures
    and still allow operations that modify the DB. Also
    verify that a MS that has suffered N+1 failures is
    still readable. Finally, verify that once all replicas
    are restarted, the MS regains quorum and allows
    write operations to succeed.
    
    Fixes a few issues found while verifying the test:
      * Reset the RPC hostlist in the event of a failure
        involving the current MS leader
      * Make SystemNotify retry certain errors
      * Add missing component authorization entries for Raft
        gRPC operations
      * Adds logic to dmg_utils.py to enable retrying after
        certain pool creation errors
      * Adds logic to server_utils.py to allow restarting
        servers without reformatting them
    
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/control/lib/control/rpc.go
+++ src/control/lib/control/rpc.go
@@ -278,7 +278,13 @@ func invokeUnaryRPC(parentCtx context.Context, log debugLogger, c UnaryInvoker, 
 		// send the request to a server that can handle the request directly.
 		rnd := rand.New(msCandidateRandSource)
 		msCandidates := hostlist.MustCreateSet("")
-		for i := 0; i < len(defaultHosts) && msCandidates.Count() < maxMSCandidates; i++ {
+
+		numCandidates := maxMSCandidates
+		if len(defaultHosts) < numCandidates {
+			numCandidates = len(defaultHosts)
+		}
+
+		for msCandidates.Count() < numCandidates {
 			if _, err := msCandidates.Insert(defaultHosts[rnd.Intn(len(defaultHosts))]); err != nil {
 				return nil, errors.Wrap(err, "failed to build MS candidates set")
 			}
@@ -288,6 +294,10 @@ func invokeUnaryRPC(parentCtx context.Context, log debugLogger, c UnaryInvoker, 
 			return nil, errors.New("unable to select MS candidates")
 		}
 	}
+
+	// Copy the starting hostlist to use for reset on retry later.
+	startHostList := make([]string, len(req.getHostList()))
+	copy(startHostList, req.getHostList())
 
 	isHardFailure := func(err error, reqCtx context.Context) bool {
 		if err == nil {
@@ -362,6 +372,10 @@ func invokeUnaryRPC(parentCtx context.Context, log debugLogger, c UnaryInvoker, 
 				if err := req.onRetry(tryCtx, try); err != nil {
 					return ur, nil
 				}
+
+				// Reset the hostlist to the starting hostlist, in order
+				// to restart the search for the current MS leader.
+				req.SetHostList(startHostList)
 				break
 			}
 
